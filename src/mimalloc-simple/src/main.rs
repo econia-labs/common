852a1e267926c11e6aa0ac8ed8db46548406418a
alnoki <43892045+alnoki@users.noreply.github.com>
2024-07-07 12:57:28 -0700

    Add simple async mimalloc crate

---
+++ src/mimalloc-simple/src/main.rs
@@ -0,0 +1,10 @@
+use mimalloc::MiMalloc;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
+#[tokio::main]
+async fn main() -> Result<(), ()> {
+    println!("Hello, world!");
+    Ok(())
+}
