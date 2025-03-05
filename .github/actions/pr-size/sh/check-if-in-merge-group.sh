d5cd613027e643c113b0f0ad12b7c5cebe9a70f0
alnoki <43892045+alnoki@users.noreply.github.com>
2025-03-04 18:08:07 -0800

    Format sh

--- .github/actions/pr-size/sh/check-if-in-merge-group.sh
+++ .github/actions/pr-size/sh/check-if-in-merge-group.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 if [ "$GITHUB_EVENT_NAME" = "merge_group" ]; then
-  exit 1
+	exit 1
 fi
--- .github/actions/pr-size/sh/get-pr-number.sh
+++ .github/actions/pr-size/sh/get-pr-number.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ -z "$PR_NUMBER" ] || [ "$PR_NUMBER" = "null" ]; then
-    exit 1
+	exit 1
 fi
 echo "PR number: $PR_NUMBER"
-echo "number=$PR_NUMBER" >> $GITHUB_OUTPUT
+echo "number=$PR_NUMBER" >>$GITHUB_OUTPUT
