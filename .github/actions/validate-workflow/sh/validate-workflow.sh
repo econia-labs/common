af8505227bf54945b0d71627b3dc7b1e00d3a44c
alnoki <43892045+alnoki@users.noreply.github.com>
2025-03-12 20:08:30 -0700

    Try final workflow name check

--- .github/actions/validate-workflow/sh/validate-workflow.sh
+++ .github/actions/validate-workflow/sh/validate-workflow.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-# Ensure calling action path is set.
-if [ -z "$CALLING_ACTION_PATH" ]; then
-  echo "::error::Calling action path is not set"
+# Ensure called action path is set.
+if [ -z "$CALLED_ACTION_PATH" ]; then
+  echo "::error::Called action path is not set"
   exit 1
 fi
 
-# Ensure the calling action path is a directory containing a workflow template.
-if [ ! -d "$CALLING_ACTION_PATH" ]; then
-    echo "::error::Calling action path not a directory: $CALLING_ACTION_PATH"
+# Ensure the called action path is a directory containing a workflow template.
+if [ ! -d "$CALLED_ACTION_PATH" ]; then
+    echo "::error::Called action path not a directory: $CALLED_ACTION_PATH"
     exit 1
 fi
-if [ ! -f "$CALLING_ACTION_PATH/workflow-template.yaml" ]; then
-    echo "::error::No `workflow-template.yaml` file found in action path"
+if [ ! -f "$CALLED_ACTION_PATH/workflow-template.yaml" ]; then
+    echo "::error::No `workflow-template.yaml` found in called action path"
     exit 1
 fi
 
@@ -21,5 +21,21 @@ if [ -z "$CALLING_WORKFLOW_REF" ]; then
   echo "::error::Calling workflow ref is not set"
   exit 1
 fi
-echo "$CALLING_WORKFLOW_REF"
 
+# Extract the workflow path from the workflow ref.
+WORKFLOW_PATH=$(echo "$CALLING_WORKFLOW_REF" | cut -d "@" -f 1)
+
+# Extract the last three directories from the calling workflow path,
+# representing the workflow's location in the repository.
+PATH_IN_REPO=$(echo "$WORKFLOW_PATH" | grep -o '/[^/]*/[^/]*/[^/]*$')
+
+# Extract the final directory name from the called action path.
+ACTION_NAME=$(basename "$CALLING_ACTION_PATH")
+
+# Construct the expected path in repo for the calling workflow.
+EXPECTED_PATH_IN_REPO="/.github/workflows/$ACTION_NAME.yaml"
+
+if [ "$PATH_IN_REPO" != "$EXPECTED_PATTERN" ]; then
+  echo "::error::Workflow path is $PATH_IN_REPO, expected $EXPECTED_PATTERN"
+  exit 1
+fi
