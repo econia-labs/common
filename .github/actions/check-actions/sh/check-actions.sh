5b61131973762066407b0fb44d0781782418c97a
alnoki <43892045+alnoki@users.noreply.github.com>
2025-03-14 14:53:40 -0700

    Tidy up

--- .github/actions/check-actions/sh/check-actions.sh
+++ .github/actions/check-actions/sh/check-actions.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-# Use yq to parse
+# Get the syntax required to properly call the workflow checker.
 CHECK_WORKFLOW_STEP=$(yq eval '.[0]' $ACTION_PATH/cfg/check-workflow-step.yaml)
 echo "Check workflow step: $CHECK_WORKFLOW_STEP"
 
@@ -10,44 +10,46 @@ cd .github/actions
 
 # Iterate over all action directories.
 for ACTION_PATH in */; do
-    # Change location to action directory.
-    cd "$ACTION_PATH"
+	# Change location to action directory.
+	cd "$ACTION_PATH"
 
-    # Verify there are no directories other than cfg and sh.
-    for DIR in */; do
-        if [ "$DIR" != "cfg/" ] && [ "$DIR" != "sh/" ]; then
-            echo "::error::Unexpected directory $DIR"
-            exit 1
-        fi
-    done
+	# Verify there are no sub-directories other than cfg and sh.
+	for DIR in */; do
+		if [ "$DIR" != "cfg/" ] && [ "$DIR" != "sh/" ]; then
+			echo "::error::Unexpected directory $DIR"
+			exit 1
+		fi
+	done
 
-    # Verify there is an action.yaml file.
-    if [ ! -f "action.yaml" ]; then
-        echo "::error::Missing action.yaml"
-        exit 1
-    fi
+	# Verify there is an action.yaml file.
+	if [ ! -f "action.yaml" ]; then
+		echo "::error::Missing action.yaml"
+		exit 1
+	fi
 
-    # Use yq to get the first element of the runs.steps array.
-    FIRST_STEP=$(yq eval '.runs.steps[0]' action.yaml)
+	# Get the first element of the `runs.steps` array in `action.yaml`.
+	FIRST_STEP=$(yq eval '.runs.steps[0]' action.yaml)
 
-    # If there is a workflow-template.yaml file, ensure check-workflow is the
-    # first step.
-    if [ -f "workflow-template.yaml" ]; then
-        if [ "$FIRST_STEP" != "$CHECK_WORKFLOW_STEP" ]; then
-            echo "::error::$ACTION_PATH first step is not check-workflow"
-            exit 1
-        fi
-    fi
+	# If there is a `workflow-template.yaml` file, ensure `action.yaml` has as
+	# its first step a call to the `check-workflow` action.
+	if [ -f "workflow-template.yaml" ]; then
+		if [ "$FIRST_STEP" != "$CHECK_WORKFLOW_STEP" ]; then
+			echo "::error::$ACTION_PATH first step is not check-workflow"
+			exit 1
+		fi
+	fi
 
-    # If the first step is check-workflow, ensure there is a
-    # workflow-template.yaml file.
-    if [ "$FIRST_STEP" = "$CHECK_WORKFLOW_STEP" ]; then
-        if [ ! -f "workflow-template.yaml" ]; then
-            echo "::error::$ACTION_PATH missing workflow-template.yaml"
-            exit 1
-        fi
-    fi
+	# If the first step in `action.yaml` is the `check-workflow` action, ensure
+	# there is a `workflow-template.yaml` file.
+	if [ "$FIRST_STEP" = "$CHECK_WORKFLOW_STEP" ]; then
+		if [ ! -f "workflow-template.yaml" ]; then
+			echo "::error::$ACTION_PATH missing workflow-template.yaml"
+			exit 1
+		fi
+	fi
 
-    # Change location back to GitHub actions directory.
-    cd ..
+	# Change location back to GitHub actions directory.
+	cd ..
 done
+
+echo "✅ All actions valid"
