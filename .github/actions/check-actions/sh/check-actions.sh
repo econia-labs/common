0c5a59de101e13571ae482e78875650156c69e3d
alnoki <43892045+alnoki@users.noreply.github.com>
2025-03-14 15:05:30 -0700

    Add escape err

--- .github/actions/check-actions/sh/check-actions.sh
+++ .github/actions/check-actions/sh/check-actions.sh
@@ -29,8 +29,21 @@ for ACTION_PATH in */; do
 	# Get the first element of the `runs.steps` array in `action.yaml`.
 	FIRST_STEP=$(yq eval '.runs.steps[0]' action.yaml)
 
-    # Ensure the first step in `action.yaml` is the `check-workflow` action.
+    # Ensure the first step in `action.yaml` is the `check-workflow` action
+    # unless explicitly overridden, for example if the action is not designed
+    # to be called by a workflow.
     if [ "$FIRST_STEP" != "$CHECK_WORKFLOW_STEP" ]; then
+
+        # Check if the string `# check-workflow: exempt` is present in the
+        # `action.yaml` file.
+        if ! grep -q "# check-workflow: exempt" action.yaml; then
+            echo "::error::$ACTION_PATH first step is not check-workflow"
+            echo "::error::If this action is not directly called by a workflow"
+            echo "::error::you can silence this check by including the comment"
+            echo "::error::<# check-workflow: exempt> in action.yaml"
+            exit 1
+        fi
+
         echo "::error::$ACTION_PATH first step is not check-workflow"
         exit 1
     fi
