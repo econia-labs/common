12605c50ac33c6796476feec1aea0f2744a85e69
alnoki <43892045+alnoki@users.noreply.github.com>
2025-03-14 13:31:12 -0700

    Add workflwo path check

--- .github/actions/validate-workflow/sh/get-workflow-path.sh
+++ .github/actions/validate-workflow/sh/get-workflow-path.sh
@@ -1,1 +1,4 @@
-echo $WORKFLOW_REF
+# Get the path of the workflow from the workflow ref.
+WORKFLOW_PATH=$(echo "$WORKFLOW_REF" | cut -d "@" -f 1 | cut -d "/" -f 3-)
+echo "Workflow path: $WORKFLOW_PATH"
+echo "workflow_path=$WORKFLOW_PATH" >>$GITHUB_OUTPUT
