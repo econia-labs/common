d105fdf4cfe006216e9c2364c10cb8058ab939dc
alnoki <43892045+alnoki@users.noreply.github.com>
2025-03-14 13:59:33 -0700

    Add pathing

--- .github/actions/validate-workflow/sh/check-workflow.sh
+++ .github/actions/validate-workflow/sh/check-workflow.sh
@@ -1,2 +1,27 @@
-cd $GITHUB_WORKSPACE
-ls -al
+# Get the name of the action from the action path.
+ACTION_NAME=$(basename "$ACTION_PATH")
+
+# Construct the expected workflow path.
+EXPECTED_PATH=".github/workflows/$ACTION_NAME.yaml"
+
+# Ensure the workflow path matches the expected path.
+if [ "$WORKFLOW_PATH" != "$EXPECTED_PATH" ]; then
+	echo "::error::Workflow at $WORKFLOW_PATH, expected $EXPECTED_PATH"
+	exit 1
+fi
+echo "Workflow path: $WORKFLOW_PATH"
+
+# Construct the workflow template path.
+WORKFLOW_TEMPLATE_PATH="$ACTION_PATH/workflow-template.yaml"
+
+# Get the workflow path inside the GitHub workspace.
+WORKFLOW_LOCAL_PATH="$GITHUB_WORKSPACE/$WORKFLOW_PATH"
+
+# Ensure workflow matches workflow template.
+if ! diff -q "$WORKFLOW_LOCAL_PATH" "$WORKFLOW_TEMPLATE_PATH" >/dev/null; then
+	echo "::error::Workflow does not match workflow template"
+	diff "$WORKFLOW_LOCAL_PATH" "$WORKFLOW_TEMPLATE_PATH"
+	exit 1
+fi
+
+echo "✅ Valid workflow limits"
