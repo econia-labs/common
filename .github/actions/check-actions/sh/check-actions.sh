acfea26ca88e8fa84c9ab974b189b43a639a2615
alnoki <43892045+alnoki@users.noreply.github.com>
2025-03-28 07:45:46 -0700

    ci(ECO-3009): Add workflow/action validation support (#27)

---
+++ .github/actions/check-actions/sh/check-actions.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+set -e
+
+# Function to print exemption instructions.
+print_exemption_instructions() {
+	echo "If you do not want this action to verify that it is called by a"
+	echo "canonical workflow defined in a template file, you can exempt this"
+	echo "step of the check by adding the following to action.yaml:"
+	echo "# check-workflow: exempt"
+}
+
+# Set the escape pattern for exempting actions from the check-workflow step.
+ESCAPE_PATTERN="# check-workflow: exempt"
+
+# Get the syntax required to properly call `check-workflow` action as a step.
+CHECK_WORKFLOW_STEP=$(yq eval '.[0]' $ACTION_PATH/cfg/check-workflow-step.yaml)
+
+# Check if repository even has an actions directory.
+ACTIONS_DIRECTORY=.github/actions
+if [ ! -d "$ACTIONS_DIRECTORY" ]; then
+	echo "::error::Missing $ACTIONS_DIRECTORY"
+	exit 1
+fi
+cd "$ACTIONS_DIRECTORY"
+
+# Check if there are any action directories.
+if [ -z "$(ls -d */ 2>/dev/null)" ]; then
+	echo "::error::No action directories found in $ACTIONS_DIRECTORY"
+	exit 1
+fi
+
+# Iterate over all action directories.
+for ACTION_DIR in */; do
+	# Change location to action directory.
+	cd "$ACTION_DIR"
+
+	# Verify there is an action.yaml file.
+	if [ ! -f "action.yaml" ]; then
+		echo "::error::Missing ${ACTION_DIR}action.yaml"
+		exit 1
+	fi
+
+	# If there are sub-directories, verify there are none besides cfg and sh.
+	if [ -n "$(ls -d */ 2>/dev/null)" ]; then
+		for SUBDIR in */; do
+			if [ "$SUBDIR" != "cfg/" ] && [ "$SUBDIR" != "sh/" ]; then
+				echo "::error::Unexpected directory ${ACTION_DIR}${SUBDIR}"
+				exit 1
+			fi
+		done
+	fi
+
+	# Unless the action is exempt, verify the first step is check-workflow and
+	# there is a workflow-template.yaml file.
+	if ! grep -q -F "$ESCAPE_PATTERN" action.yaml; then
+
+		# Get the first element of the `runs.steps` array in `action.yaml`.
+		FIRST_STEP=$(yq eval '.runs.steps[0]' action.yaml)
+
+		# Ensure the first step in `action.yaml` is the `check-workflow`.
+		if [ "$FIRST_STEP" != "$CHECK_WORKFLOW_STEP" ]; then
+			ACTION="${ACTION_DIR}action.yaml"
+			echo "::error::$ACTION missing check-workflow as first step"
+			print_exemption_instructions
+			exit 1
+		fi
+
+		# Ensure there is a template workflow file.
+		if [ ! -f "workflow-template.yaml" ]; then
+			echo "::error::${ACTION_DIR}workflow-template.yaml missing"
+			print_exemption_instructions
+			exit 1
+		fi
+
+	fi
+
+	# Change location back to GitHub actions directory.
+	cd ..
+done
+
+echo "✅ All actions valid"
---
+++ .github/actions/check-workflow/sh/check-workflow.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -e
+
+# Get the name of the action that the workflow is calling.
+ACTION_NAME=$(basename "$ACTION_PATH")
+
+# Construct the expected workflow path.
+EXPECTED_PATH=".github/workflows/$ACTION_NAME.yaml"
+
+# Ensure the workflow path matches the expected path.
+if [ "$WORKFLOW_PATH" != "$EXPECTED_PATH" ]; then
+	echo "::error::Workflow at $WORKFLOW_PATH, expected $EXPECTED_PATH"
+	exit 1
+fi
+
+# Construct the workflow template path.
+WORKFLOW_TEMPLATE_PATH="$ACTION_PATH/workflow-template.yaml"
+
+# Get workflow path inside the GitHub workspace for the checked out workflow.
+WORKFLOW_LOCAL_PATH="$CHECKOUT_WORKSPACE/$WORKFLOW_PATH"
+
+# Ensure workflow matches workflow template.
+if ! diff -q "$WORKFLOW_LOCAL_PATH" "$WORKFLOW_TEMPLATE_PATH" >/dev/null; then
+	echo "::error::$WORKFLOW_PATH does not match template, see diff below:"
+	diff "$WORKFLOW_LOCAL_PATH" "$WORKFLOW_TEMPLATE_PATH"
+	exit 1
+fi
+
+echo "✅ Valid workflow"
